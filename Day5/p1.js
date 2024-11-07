6c1aa95f1c51940707df02b15e875370314e7cfc
krishnasharmaaa <sharmakrishna0441@gmail.com>
2024-11-07 22:14:23 +0530

    Day5 Work

---
+++ Day5/p1.js
@@ -0,0 +1,26 @@
+// let x='12';
+// let num=Number(x);
+// console.log(typeof num);
+// function display(){
+//     // let c=confirm("are you sure to logout:");
+//     let c=prompt("enter your name:");
+//     alert(c);
+// }
+
+// let a=12;
+// let b='12';
+// console.log(a==b);
+// console.log(a===b);
+
+// let sayhello=()=>{
+//  console.log("arrow function:");   
+// };
+
+// let sayhello = () => console.log("Arrow function");
+// sayhello();
+let sum=(x,y)=>{
+    return x+y;
+};
+let res=sum(12,12);
+console.log(res);
+// console.log(sum(12,12));
